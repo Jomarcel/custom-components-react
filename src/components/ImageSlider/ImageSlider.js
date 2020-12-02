23b686e019e19e42a8dd53ae18446b1f6715e44c
Jomarcel <jomarcel_n@outlook.com>
2020-12-03 01:41:29 +1000

    added image data and near complete functions to change image slides

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -4,12 +4,24 @@ import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 
 const ImageSlider = ({ slides }) => {
   const [currentImage, setCurrentImage] = useState(0);
-  //   const length = slides.length;
-  //   console.log(slides);
+  const length = slides.length;
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    console.log("image data is null");
+    return null;
+  }
+  const nextSlide = () => {
+    setCurrentImage(currentImage === length - 1 ? 0 : currentImage + 1);
+  };
+  const prevSlide = () => {
+    setCurrentImage(currentImage === 0 ? length - 1 : currentImage - 1);
+  };
+  console.log("currentImage:", currentImage);
+  console.log("length:", length);
   return (
     <div className="slider">
-      <FaArrowCircleRight className="slider__RightArrow" onClick={{}} />
-      <FaArrowCircleLeft className="slider__leftArrow" onClick={{}} />
+      <FaArrowCircleRight className="slider__RightArrow" onClick={nextSlide} />
+      <FaArrowCircleLeft className="slider__LeftArrow" onClick={prevSlide} />
 
       {sliderData.map((slide, idx) => {
         return <img src={slide.image} alt="travel-image" />;
