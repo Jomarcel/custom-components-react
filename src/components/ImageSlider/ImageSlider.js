c1304fd02512f03d3d53f140fcb670f2abcc4500
Jomarcel <jomarcel_n@outlook.com>
2020-12-08 13:39:34 +1000

    refactor code

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -3,7 +3,7 @@ import { sliderData } from "./SliderData";
 import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 
 const ImageSlider = ({ slides }) => {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [currentImageIdx, setCurrentImageIdx] = useState(0);
   const length = slides.length;
 
   if (!Array.isArray(slides) || slides.length <= 0) {
@@ -11,12 +11,16 @@ const ImageSlider = ({ slides }) => {
     return null;
   }
   const nextSlide = () => {
-    setCurrentImage(currentImage === length - 1 ? 0 : currentImage + 1);
+    setCurrentImageIdx(
+      currentImageIdx === length - 1 ? 0 : currentImageIdx + 1
+    );
   };
   const prevSlide = () => {
-    setCurrentImage(currentImage === 0 ? length - 1 : currentImage - 1);
+    setCurrentImageIdx(
+      currentImageIdx === 0 ? length - 1 : currentImageIdx - 1
+    );
   };
-  console.log("currentImage:", currentImage);
+  console.log("currentImageIdx:", currentImageIdx);
   console.log("length:", length);
   return (
     <div className="slider">
@@ -28,14 +32,12 @@ const ImageSlider = ({ slides }) => {
           return (
             <div
               className={
-                idx === currentImage ? "slider__active" : "slider__slide"
+                idx === currentImageIdx ? "slider__active" : "slider__slide"
               }
-              // className="slider__active"
-              // key={idx}
             >
-              {console.log(idx)}
+              {/* {console.log(idx)} */}
 
-              {idx === currentImage && (
+              {idx === currentImageIdx && (
                 <img src={slide.image} alt="travel-image" className="image" />
               )}
             </div>
